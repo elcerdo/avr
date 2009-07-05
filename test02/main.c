b578f8230cbec1cc5699a8c2917d63838ac9ff44
elcerdo <georges.wbushjr@laposte.net>
2009-07-05 23:45:50 +0200

    final test02

--- test02/main.c
+++ test02/main.c
@@ -83,29 +83,33 @@ void toggle_led(void) {
 	if (bit_is_set(PORTB,PSTATUS)) PORTB &= ~_BV(PSTATUS);
 	else PORTB |= _BV(PSTATUS);
 }
-	
+
+#define PAUDIO PB3
+
+static uint8_t tone = 0x80;
+
 ISR(TIMER0_COMP_vect) {
-	OCR0 += 4;
+	if (OCR0 < tone) OCR0++;
+	if (OCR0 > tone) OCR0--;
 }
 
 int main(void) {
-	uint32_t k,l;
+	uint32_t l;
 	uint8_t i;
 
 	cli();
 	
 	//set pins as output
-	DDRA = _BV(DDA0) | _BV(DDA1) | _BV(DDA3) | _BV(DDA4) | _BV(DDA5) | _BV(DDA6);
-	DDRB = _BV(DDB0) | _BV(DDB3);
+	DDRA = _BV(PSDATA) | _BV(PSCLEAR) | _BV(PRLOAD) | _BV(PRCLOCK) | _BV(PSCLOCK);
+	DDRB = _BV(PSTATUS) | _BV(PAUDIO);
 
 	//light dev board led
 	PORTA |= _BV(PRLOAD);
-	PORTA &= _BV(PA4);
 	PORTB &= ~_BV(PB0);
 
 	//setup timer 0
 	TCCR0 = _BV(WGM01) | _BV(CS02) | _BV(CS00) | _BV(COM00);
-	OCR0 = 0x70;
+	OCR0 = tone;
 	TIMSK = _BV(OCIE0);
 
 	keypad = malloc(2);
@@ -114,10 +118,25 @@ int main(void) {
 
 	while (1) {
 		update_keypad();
+
+		//display keypad
 		i = (keypad[0] & 0x0f) | ((keypad[1] & 0x0f) << 4);
 		sel_load_value(~i);
+
+		//select tone
+		if (bit_is_set(keypad[0],0)) tone=0xff;
+		if (bit_is_set(keypad[0],1)) tone=0x80;
+		if (bit_is_set(keypad[0],2)) tone=0x40;
+		if (bit_is_set(keypad[0],3)) tone=0x20;
+
+		//select scale
+		if (bit_is_set(keypad[1],0)) { TCCR0 |= _BV(CS02); TCCR0 &= ~_BV(CS01); TCCR0 |= _BV(CS00); }
+		if (bit_is_set(keypad[1],1)) { TCCR0 |= _BV(CS02); TCCR0 &= ~_BV(CS01); TCCR0 &= ~_BV(CS00); }
+		if (bit_is_set(keypad[1],2)) { TCCR0 &= ~_BV(CS02); TCCR0 |= _BV(CS01); TCCR0 |= _BV(CS00); }
+		if (bit_is_set(keypad[1],3)) { TCCR0 &= ~_BV(CS02); TCCR0 |= _BV(CS01); TCCR0 &= ~_BV(CS00); }
+
 		toggle_led();
-		for (l=0; l<10000; l++) { }
+		for (l=0; l<5000; l++) { }
 	}
 
 	cli();
